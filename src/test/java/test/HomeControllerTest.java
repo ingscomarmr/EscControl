e3e0c04ffc41ae268278e1d83cd851bdc57963a8
omunguia <ingscomarmr@gmail.com>
2016-11-23 16:20:02 -0600

    Primera version de spring mvc
    Primera version de spring MCV - inicializacion de configuración para
    iniciar
    http://www.uv.es/grimo/teaching/SpringMVCv4PasoAPaso/part1.html

---
+++ src/test/java/test/HomeControllerTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bartlett.esccontrol.web.HomeController;
+
+public class HomeControllerTest {
+	@Test
+    public void testHandleRequestView() throws Exception{		
+		/*
+        HomeController controller = new HomeController();
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+        System.out.println("Controller:" + modelAndView.getViewName());
+        assertEquals("home_view", modelAndView.getViewName());*/
+    }
+}
